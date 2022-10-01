c30f170e42b1a3dfb2bc5a4cf71a3aacb4dc0ae6
zerleger <101527159+zerleger@users.noreply.github.com>
2022-10-01 16:51:51 +0200

    kommentare

--- .history/main/main_20221001161907.cpp
+++ .history/main/main_20221001161907.cpp
@@ -13,9 +13,9 @@ bool Recursive = false;
 bool CaseInsensitive = false;
 
 
-namespace fs = std::filesystem; //abkuerzung fuer filesystem namespace
+namespace fs = std::filesystem;
 
-bool iequals(const std::string& a, const std::string& b) //Hilfsfunktion zum Vergleichen von Strings ungeachtet der Groß- oder Kleinschreibung
+bool iequals(const std::string& a, const std::string& b)
 {
     unsigned int sz = a.size();
     if (b.size() != sz)
@@ -26,27 +26,16 @@ bool iequals(const std::string& a, const std::string& b) //Hilfsfunktion zum Ver
     return true;
 }
 
-/*
-Funktion die den Inhalt des Verzeichnisses im directory vector speichert
-Die Variable "path" ist der Pfad in dem die Funktion nach der Datei suchen soll, "file" ist der Name der Datei.
-*/
-
-int getDir(std::string path,std::string& file,std::vector<fs::directory_entry>& directory) { 
+int getDir(std::string path,std::string& file,std::vector<fs::directory_entry>& directory) {
     for (auto &entry : fs::directory_iterator(path)){
         if(!CaseInsensitive && entry.path().filename() == file){
-            /* 
-            sollten der Name der gefundenen Datei gleich dem Namen der gesuchten Datei sein, dann returned die Funktion 1
-            Dieser Rückgabewert wird in der childSearch Funktion abgefangen und beendet das Suchen.
-            */
             return 1;
         }
         if(CaseInsensitive && iequals(entry.path().filename(),file)){
-            // analog zu oben nur case-insensitive
             file = entry.path().filename();
             return 1;
         }
         if(entry.is_directory()){
-            // jegliche Verzeichnisse die gefunden werden, werden in den directory vector gespeichert, um in ihnen rekursiv suchen zu können
             directory.push_back(entry);
         }
     }
@@ -57,18 +46,16 @@ int getDir(std::string path,std::string& file,std::vector<fs::directory_entry>& 
 
 void childSearch(std::string path,std::string& file,bool& found) {
     if(found){
-        return; 
-        // Falls der found boolean true ist wird die Suche in allen rekursiven Aufrufen dieses Prozesses beendet. 
-        // (Um unnötiges Suchen zu vermeiden und die Rechenzeit zu verkürzen)
+        return;
     }
     std::vector<fs::directory_entry> directory;
-    if(getDir(path,file,directory)){ // Falls returnwert == 1 wurde die Datei gefunden
+    if(getDir(path,file,directory)){
         pid_t pid = getpid();
         std::cout << pid << ": " << file << ": " << path + '/' + file << "\n";
         found = true;
     }
     else{
-        if(directory.size() != 0 && Recursive){ // Falls 0 wird die Suche in den Unterverzeichnissen fortgesetzt
+        if(directory.size() != 0 && Recursive){
             for(auto& entry: directory){
                 childSearch(entry.path(),file, found);
             }
@@ -78,11 +65,11 @@ void childSearch(std::string path,std::string& file,bool& found) {
 }
 
 void processEngine(std::string initialPath, std::vector<std::string> files) {
-    pid_t ppid = getpid(); // Parent PID wird gespeichert
+    pid_t ppid = getpid();
     pid_t childpid;
 
     for (int i = 0; i < files.size(); i++) {
-        if (getpid() == ppid) { // Falls die PID == PPID dann soll geforkt werden, alle Prozesse werden ausschließlich vom Parent geforkt
+        if (getpid() == ppid) {
             childpid = fork();
             if (getpid() == ppid)
                 continue;
@@ -95,11 +82,11 @@ void processEngine(std::string initialPath, std::vector<std::string> files) {
             break;
         case 0:
             // children
-            childSearch(initialPath,files[i],found); // Falls PID == 0 => wir befinden uns im Child Prozess d.h. wir können die Suche nach den Dateien beginnen.
+            childSearch(initialPath,files[i],found);
             if(!found){
                 //std::cout << "file " + files[i] + " not found\n";
             }
-            exit(0); // Terminiert den Child Prozess
+            exit(0);
             break;
         default:
             break;
@@ -109,7 +96,7 @@ void processEngine(std::string initialPath, std::vector<std::string> files) {
 
     if (getpid() == ppid) {
         for(int i = 0; i < files.size();i++){
-            wait(NULL); // Parent Prozess wartet bis alle Child Prozesse terminiert wurden
+            wait(NULL);
         }
 
     }
@@ -144,28 +131,24 @@ void parseCLI(int argc, char** argv,int& flags){
 
 int main(int argc, char** argv)
 {
-    int flags = 0; // speichert die Anzahl der gesetzten Flags
+    int flags = 0;
 
     try{
         parseCLI(argc,argv,flags);
     }catch(int i){
         switch(i){
             case 1:
-            std::cout << "Invalid Flag\n"; // Throws Error falls eine der Flags ungültig ist
+            std::cout << "Invalid Flag\n";
             return 1;
         }
         
     }
 
-    std::string searchpath; 
+    std::string searchpath;
     int argIndex = 0;
-    /* 
-    Da getopt das argv array, nach dem parsen, so sortiert, dass die Flags immer nach dem Programmnamen (immer argv[0]) aber vor dem Suchpfad und Dateinamen stehen
-    muss zwischen der Anzahl der Flags im switch-case unterschieden werden, weil die Indexierung des Suchpfades und der Dateinamen im argv array davon abhängt.
-    */
     switch(flags){
         case 0:
-            searchpath = argv[1]; 
+            searchpath = argv[1];
             argIndex = 2;
             break;
         case 1:
@@ -183,10 +166,10 @@ int main(int argc, char** argv)
     std::vector<std::string> files;
 
     for(; argIndex < argc; argIndex++){
-        files.push_back(argv[argIndex]); // alle Dateinamen nach welchen gesucht werden soll werden im "files" Vektor abgespeichert
+        files.push_back(argv[argIndex]);
     }
 
-    processEngine(searchpath,files); //processEngine startet den Abspaltungsvorgang
+    processEngine(searchpath,files);
     
 
     return 0;
